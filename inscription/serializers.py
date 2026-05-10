450e4fbd69bb2c12dbd94eb2920e5edafd81de64
like2300 <elengaomerfils@gmail.com>
2026-05-10 13:31:05 +0100

    v5

--- inscription/serializers.py
+++ inscription/serializers.py
@@ -3,25 +3,20 @@ from django.conf import settings
 from .models import Inscription
 
 class InscriptionSerializer(serializers.ModelSerializer):
-    photo = serializers.SerializerMethodField()
-
     class Meta:
         model = Inscription
         fields = '__all__'
 
-    def get_photo(self, obj):
-        if not obj.photo:
-            return None
-            
-        photo_url = obj.photo.url
-        if photo_url.startswith('http'):
-            return photo_url.replace('http://', 'https://') if 'alwaysdata.net' in photo_url else photo_url
-            
-        request = self.context.get('request')
-        if request:
-            full_url = request.build_absolute_uri(photo_url)
-            if 'alwaysdata.net' in full_url:
-                full_url = full_url.replace('http://', 'https://')
-            return full_url
-            
-        return f"https://estim-campus.alwaysdata.net{photo_url}"
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        if instance.photo:
+            photo_url = instance.photo.url
+            request = self.context.get('request')
+            if request:
+                full_url = request.build_absolute_uri(photo_url)
+                if 'alwaysdata.net' in full_url:
+                    full_url = full_url.replace('http://', 'https://')
+                ret['photo'] = full_url
+            else:
+                ret['photo'] = f"https://estim-campus.alwaysdata.net{photo_url}"
+        return ret
